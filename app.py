e0ead7d82dd05cad41cfd6210361ec7faf52188b
yama-kei <keisuke.yamaguchi@nifty.ne.jp>
2022-05-10 21:17:33 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,39 @@
+import os
+
+from flask import Flask
+from flask import request
+from flask import render_template
+
+
+from positiongen import generate_positions
+
+app = Flask(__name__)
+
+positions = {
+    'First Base':'',
+    'Second Base':'',
+    'Third Base':'',
+    'Pitcher':'',
+    'Short Stop':'',
+    'Cather':'',
+    'Right Field':'',
+    'Left Field':'',
+    'Center Field':''
+}
+
+@app.route('/')
+def mainpage():
+    return render_template('index.html',
+    position_details=positions,
+    team='')
+
+@app.route('/generate', methods=['POST'])
+def generate():
+    positions = request.form.getlist('positions')
+    players = request.form.getlist('players')
+    team = request.form.get('team')
+    innings = int(request.form.get('innings'))
+    randomize = request.form.get('randomize')
+    (order, position_map) = generate_positions(positions, players, innings)
+    position_data = [[p]+pl for p,pl in position_map.items()]
+    return render_template('generate.html', team=team, innings=innings, positions_details=position_data, order=order)
