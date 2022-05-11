15667d3e3830f8a83e59fbfba0ad9ed5a71c9211
yama-kei <keisuke.yamaguchi@nifty.ne.jp>
2022-05-11 10:48:25 -0700

    implemented order preservation cosmetic changes

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-
-
 from positiongen import generate_positions
 
 app = Flask(__name__)
@@ -31,7 +29,9 @@ def generate():
     players = request.form.getlist('players')
     team = request.form.get('team')
     innings = int(request.form.get('innings'))
-    randomize = request.form.get('randomize')
-    (order, position_map) = generate_positions(positions, players, innings)
+    norandomize = True if request.form.get('norandomize') == 'on' else False
+    keeporder = True if request.form.get('keeporder') == 'on' else False
+    (order, position_map) = generate_positions(
+        positions, players, innings, norandomize, keeporder)
     position_data = [[p]+pl for p,pl in position_map.items()]
     return render_template('generate.html', team=team, innings=innings, positions_details=position_data, order=order)
--- positiongen.py
+++ positiongen.py
@@ -1,12 +1,24 @@
 import random
 
 
-def generate_positions(positions, players, innings, randomize=True):
-    random.shuffle(players)
+def dedupe_and_remove_empty(listobj):
+    result = []
+    [result.append(item) for item in listobj if item not in result and item != '']
+    return result
+
+def generate_positions(positions, players, innings, norandomize, keeporder):
+    players = dedupe_and_remove_empty(players)
+    positions = dedupe_and_remove_empty(positions)
     fieldingpos = {pos:[] for pos in positions}
-    fielding = players[:len(positions)]
+    fielding = players
     available = positions[:len(fielding)]
+    if norandomize:
+        for c, pos in enumerate(available):
+            fieldingpos[pos].append(fielding[c])
+        innings -= 1
     random.shuffle(available)
+    if not keeporder:
+        random.shuffle(players)
     for i in range(innings):
         for c, pos in enumerate(available):
             fieldingpos[pos].append(fielding[c])
