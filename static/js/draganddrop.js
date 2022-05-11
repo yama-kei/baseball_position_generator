66728ad54584fa3e494f13eb737725a6a324d253
yama-kei <keisuke.yamaguchi@nifty.ne.jp>
2022-05-10 21:28:45 -0700

    add application files and deployment files for Heroku

---
+++ static/js/draganddrop.js
@@ -0,0 +1,28 @@
+$(document).ready(function () {
+  var $tableBody = $('#recipeTableBody');
+  var $menu = $('#menu');
+    $(document).on('click', '.recipe-table__add-row-btn', function (e) {
+        var $el = $(e.currentTarget);
+        var htmlString = $('#rowTemplate').html();
+        $tableBody.append(htmlString);
+        return false;
+    });
+
+    $tableBody.on('click', '.recipe-table__del-row-btn', function (e) {
+        var $el = $(e.currentTarget);
+        var $row = $el.closest('tr');
+        $row.remove();
+        return false;
+    });
+  Sortable.create(
+        $tableBody[0],
+        {
+            animation: 150,
+            scroll: true,
+            handle: '.drag-handler',
+            onEnd: function () {
+                console.log('More see in https://github.com/RubaXa/Sortable');
+            }
+        }
+    );
+});
