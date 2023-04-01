52b2f3c41e1a5955c3380d58a6d9630a5eee2631
yamakei <keisuke.yamaguchi@gmail.com>
2023-04-01 08:31:16 -0700

    add comment for the main function

--- positiongen.py
+++ positiongen.py
@@ -4,17 +4,26 @@ from datetime import datetime
 
 
 def dedupe_and_remove_empty(listobj):
+    """
+    """
     result = []
     [result.append(item) for item in listobj if item not in result and item != '']
     return result
 
 def generate_positions(positions, players, innings, norandomize, keeporder):
+    """
+    This function returns tuple of players (batting order) and two-dimensional list
+    consisting of positions and corresponding players for all innings
+    list of positions, players are given
+    positions are kept for the first inning if norandomize option is set
+    batting order is preserved if keeporder option is set
+    """
+    random.seed(datetime.now().timestamp())
     players = dedupe_and_remove_empty(players)
     positions = dedupe_and_remove_empty(positions)
     fieldingpos = {pos:[] for pos in positions}
     fielding = copy.deepcopy(players)
     available = positions[:len(fielding)]
-    random.seed(datetime.now().timestamp())
     if norandomize:
         for c, pos in enumerate(available):
             fieldingpos[pos].append(fielding[c])
